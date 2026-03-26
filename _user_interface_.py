a6baea9738aaf3ce7a698df327fd1f6f1fe2362d
favtripreporting <favtripreporting@gmail.com>
2026-03-26 12:33:35 -0500

    Change formatting of per key help text

--- _user_interface_.py
+++ _user_interface_.py
@@ -504,11 +504,12 @@ def render_run_form(cfg):
 
             st.caption("""
               Map **Store, Report Key → Emails (comma)**.
+              
               **Email Delivery Priority:**  
-              - `(Store+Key)` → First priority  
-              - `(, Key)` → Second priority  
-              - `(Store,)` → Third priority  
-              - `()` → Default
+              - `(Store, Key)` → First priority set of emails  
+              - `(, Key)` → Second priority set of emails  
+              - `(Store,)` → Third priority set of emails
+              - If not defined, it will use the default set of emails in `To (comma)` field above
               """)
         
             rows = []
