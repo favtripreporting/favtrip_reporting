d4015dcdf0251d71b7e57a786a05620d27da7d57
Ryan J Morrow <ryan-morrow@uiowa.edu>
2026-03-29 21:11:18 -0500

    Added Error Gating v6

--- core_functional_modules/gmail_utils.py
+++ core_functional_modules/gmail_utils.py
@@ -259,7 +259,9 @@ def email_error_report(
 
     msg.set_content(
         f"Hi Technical Support Team,\n"
-        f"A user tried using the reporting pipeline, however some of the items that were uploaded are not listed on the Vendor Price Book. The default recipient of the pipeline run is CC'd on this email for visibility and communication purposes. Please reply to this email once the Vendor Price Book is updated so that the user knows they can rerun the pipeline.\n"
+        f"A user tried using the reporting pipeline, however some of the items that were uploaded are not listed on the Vendor Price Book.\n"
+        f"The default recipient of the pipeline run is CC'd on this email for visibility and communication purposes.\n"
+        f"Please reply to this email once the Vendor Price Book is updated so that the user knows they can rerun the pipeline.\n\n"
         f"Google Sheet: {sheet_link}\n"
         f"Vendor Price Book: {vendor_price_book_link}"
         f"Attached: {pdf_name}\n"
@@ -269,7 +271,10 @@ def email_error_report(
     msg.add_alternative(
         f"""
         <p>Hi Technical Support Team,</p>
-        <p>A user tried using the reporting pipeline, however some of the items that were uploaded are not listed on the Vendor Price Book. The default recipient of the pipeline run is CC'd on this email for visibility and communication purposes. Please reply to this email once the Vendor Price Book is updated so that the user knows they can rerun the pipeline.</p>
+        <p>A user tried using the reporting pipeline, however some of the items that were uploaded are not listed on the Vendor Price Book.</p>
+        <p>The default recipient of the pipeline run is CC'd on this email for visibility and communication purposes.</p>
+        <p>Please reply to this email once the Vendor Price Book is updated so that the user knows they can rerun the pipeline.</p>
+        <p></p>
         <p><a href="{sheet_link}">Open Error Report in Google Sheets</a></p>
         <p><a href="{vendor_price_book_link}">Edit Vendor Price Book in Google Sheets</a></p>
         <p>Attached: {pdf_name}</p>
